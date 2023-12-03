a1f7e519785a9e21daab4e87b70635524d919e37
Samujjwal Kumar <kumarsamujjwal@gmail.com>
2023-12-03 05:19:11 -0500

    Tried background loading

--- Scripts/level_select.gd
+++ Scripts/level_select.gd
@@ -3,7 +3,7 @@ const LEVEL_BTN=preload("res://Scenes/level_btn.tscn")
 @export_dir var dir_path
 @onready var grid= $MarginContainer/VBoxContainer/GridContainer
 
-
+var clicked_button = ""
 
 #func _ready():
 	#Gets directory
@@ -46,40 +46,46 @@ func _on_level_select_backbtn_pressed():
 
 
 func _on_level_1_pressed():
+	clicked_button = "world.tscn"
 	get_tree().change_scene_to_file("res://Levels/world.tscn")
 
 
 func _on_level_2_pressed():
+	clicked_button = "Level_2.tscn"
 	get_tree().change_scene_to_file("res://Levels/Level_2.tscn")
 
 
-
+func _on_level_3_pressed():
+	clicked_button = "Level_3.tscn"
+	get_tree().change_scene_to_file("res://Levels/Level_3.tscn")
 
 func _on_level_4_pressed():
+	clicked_button = "Level_4.tscn"
 	get_tree().change_scene_to_file("res://Levels/Level_4.tscn")
 
 
-func _on_level_3_pressed():
-	get_tree().change_scene_to_file("res://Levels/Level_3.tscn")
-
-
 func _on_level_5_pressed():
+	clicked_button = "Level_5.tscn"
 	get_tree().change_scene_to_file("res://Levels/Level_5.tscn")
 	
 
-
 func _on_level_6_pressed():
+	clicked_button = "Level_6.tscn"
 	get_tree().change_scene_to_file("res://Levels/Level_6.tscn")
 	
 func _on_level_7_pressed():
+	clicked_button = "Level_7.tscn"
 	get_tree().change_scene_to_file("res://Levels/Level_7.tscn")
 
 func _on_level_8_pressed():
+	clicked_button = "Level_8.tscn"
 	get_tree().change_scene_to_file("res://Levels/Level_8.tscn")
 
 func _on_level_9_pressed():
+	clicked_button = "Level_9.tscn"
 	get_tree().change_scene_to_file("res://Levels/Level_9.tscn")
 
 
 func _on_level_10_pressed():
+	clicked_button = "Level_10.tscn"
 	get_tree().change_scene_to_file("res://Levels/Level_10.tscn")
